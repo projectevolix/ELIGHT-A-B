ece44e9180f06cba108056bca1fcc2fdeef0022e
vidura <vidura200427@gmail.com>
2025-11-06 00:16:32 +0530

    feat: logger impl done

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import { DB_URI } from '../config/env.config';
+import { logger } from '../config/logger.config';
 
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(DB_URI);
-    console.log('MongoDB connected successfully.');
+    logger.info('MongoDB connected successfully.');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    logger.error('MongoDB connection error:', { error });
     throw error; 
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import app from "./app";
 import { PORT } from "./config/env.config";
+import { logger } from "./config/logger.config";
 import connectToDatabase from "./database/mongodb";
+
+// --- CATCH UNHANDLED ERRORS ---
+// This catches sync errors
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION! 💥 Shutting down...', {
+    stack: err.stack,
+  });
+  process.exit(1);
+});
+
+// This catches async errors (unhandled promise rejections)
+process.on('unhandledRejection', (err: Error) => {
+  logger.error('UNHANDLED REJECTION! 💥 Shutting down...', {
+    message: err.message,
+    stack: err.stack,
+  });
+  process.exit(1);
+});
 
 // PORT is already a number from env.config.ts
 const port = PORT;
